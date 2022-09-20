4eae086a723acfa93da4da37394a3977cdbf4d62
Andrii Stepaniuk <andrewvstepanyuk@gmail.com>
2022-09-20 11:21:40 +0300

    solution task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+// В HTML есть кнопки «Start» и «Stop».
 
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
+
+// ВНИМАНИЕ
+// Учти, на кнопку «Start» можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    const containerEl = document.querySelector('body');
+    const startBtn = document.querySelector('[data-start]');
+    const stopBtn = document.querySelector('[data-stop]');
+
+    let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+    containerEl.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    }, 2000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    
+});
